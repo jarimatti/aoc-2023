9e0165ff50ae69a2782af19e01db7e456e0bfa07
Jarimatti Valkonen <jarimatti@me.com>
2023-12-02 11:12:05 +0000

    Remove initial test.

--- lib/aoc2023.ex
+++ lib/aoc2023.ex
@@ -1,18 +1,2 @@
 defmodule Aoc2023 do
-  @moduledoc """
-  Documentation for `Aoc2023`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Aoc2023.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
