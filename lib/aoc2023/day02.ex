924dbdbc53ae03ea5b0b8c2c78c5edb40029b28e
Jarimatti Valkonen <jarimatti@me.com>
2023-12-03 01:15:58 +0000

    Day 2, part 1 solved.

---
+++ lib/aoc2023/day02.ex
@@ -0,0 +1,76 @@
+defmodule Aoc2023.Day02 do
+  def input() do
+    File.read!("input/day02.txt")
+  end
+
+  def part1(data) do
+    data
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.reject(&empty_string?/1)
+    |> Enum.map(&parse_game/1)
+    |> Enum.filter(fn game -> is_game_possible(game, part1_bag()) end)
+    |> Enum.map(fn {{:id, id}, _} -> id end)
+    |> Enum.sum()
+  end
+
+  defp part1_bag() do
+    %{
+      red: 12,
+      green: 13,
+      blue: 14
+    }
+  end
+
+  defp is_game_possible({_, moves}, bag) do
+    Enum.all?(moves, fn move -> is_move_possible(move, bag) end)
+  end
+
+  defp is_move_possible(move, bag) do
+    Map.get(move, :red, 0) <= bag.red and
+      Map.get(move, :green, 0) <= bag.green and
+      Map.get(move, :blue, 0) <= bag.blue
+  end
+
+  defp empty_string?(""), do: true
+  defp empty_string?(_), do: false
+
+  defp parse_game(line) do
+    [id_part, moves_part] = String.split(line, ":")
+    game_id = parse_id(id_part)
+
+    moves =
+      moves_part
+      |> String.split(";")
+      |> Enum.map(&parse_move/1)
+
+    {game_id, moves}
+  end
+
+  defp parse_id("Game " <> id) do
+    {:id, String.to_integer(id)}
+  end
+
+  defp parse_move(string) do
+    string
+    |> String.split(",")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&parse_cube_count/1)
+    |> Map.new()
+  end
+
+  defp parse_cube_count(string) do
+    [number_string, colour_string] = String.split(string)
+
+    count = String.to_integer(number_string)
+
+    colour =
+      case colour_string do
+        "blue" -> :blue
+        "red" -> :red
+        "green" -> :green
+      end
+
+    {colour, count}
+  end
+end
