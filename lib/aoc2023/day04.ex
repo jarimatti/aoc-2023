731d7d09417c3f9c85899cea1ec408adf24e916f
Jarimatti Valkonen <jarimatti@me.com>
2023-12-04 08:09:39 +0000

    Day 4, part 2 solved.

--- lib/aoc2023/day04.ex
+++ lib/aoc2023/day04.ex
@@ -7,13 +7,20 @@ defmodule Aoc2023.Day04 do
     data
     |> parse_cards()
     |> Map.values()
+    |> Enum.map(&matching_number_count/1)
     |> Enum.map(&card_score/1)
     |> Enum.sum()
   end
 
   def part2(data) do
-    data
-    |> parse_cards()
+    card_and_count =
+      data
+      |> parse_cards()
+      |> Map.new(fn {k, v} -> {k, matching_number_count(v)} end)
+
+    stack = Map.keys(card_and_count)
+
+    total_cards(stack, 0, card_and_count)
   end
 
   defp parse_cards(data) do
@@ -32,8 +39,8 @@ defmodule Aoc2023.Day04 do
     [id_string, numbers] = String.split(line, ":")
 
     id =
-      case id_string do
-        "Card " <> id -> String.to_integer(id)
+      case String.split(id_string) do
+        ["Card", s] -> String.to_integer(s)
       end
 
     [winning_numbers, card_numbers] = String.split(numbers, " | ")
@@ -50,15 +57,28 @@ defmodule Aoc2023.Day04 do
     |> Enum.map(&String.to_integer/1)
   end
 
-  defp card_score({winning, card}) do
-    same_number_count =
-      winning
-      |> MapSet.intersection(card)
-      |> MapSet.size()
+  defp matching_number_count({winning, card}) do
+    winning
+    |> MapSet.intersection(card)
+    |> MapSet.size()
+  end
 
-    case same_number_count do
-      0 -> 0
-      n -> 2 ** (n - 1)
+  defp card_score(0), do: 0
+  defp card_score(n) when n > 0, do: 2 ** (n - 1)
+
+  defp total_cards([], n, _), do: n
+
+  defp total_cards([id | rest], n, card_counts) do
+    matching_numbers = Map.get(card_counts, id)
+    next = next_cards(id, matching_numbers)
+    total_cards(next ++ rest, n + 1, card_counts)
+  end
+
+  defp next_cards(_, 0), do: []
+
+  defp next_cards(current, count) when count > 0 do
+    for step <- 1..count do
+      current + step
     end
   end
 end
