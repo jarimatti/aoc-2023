cefa947a46f81d45f76643c77300c1311f3af472
Jarimatti Valkonen <jarimatti@me.com>
2023-12-08 07:46:09 +0000

    Day 7 part 1 stub.

---
+++ lib/aoc2023/day07.ex
@@ -0,0 +1,6 @@
+defmodule Aoc2023.Day07 do
+  def part1(_data) do
+    # stub
+    0
+  end
+end
---
+++ test/aoc2023/day07_test.exs
@@ -0,0 +1,19 @@
+defmodule Aoc2023.Day07Test do
+  use ExUnit.Case, async: true
+
+  alias Aoc2023.Day07
+
+  def data() do
+    """
+    32T3K 765
+    T55J5 684
+    KK677 28
+    KTJJT 220
+    QQQJA 483
+    """
+  end
+
+  test "part 1 sample data" do
+    assert Day07.part1(data()) == 6440
+  end
+end
