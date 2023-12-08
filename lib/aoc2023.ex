e9f7084ac1c7500ddfbf46e53db30bf103e55223
Jarimatti Valkonen <jarimatti@me.com>
2023-12-08 22:56:25 +0000

    Day 8, part 1 complete.

--- lib/aoc2023.ex
+++ lib/aoc2023.ex
@@ -1,2 +1,6 @@
 defmodule Aoc2023 do
+
+  def empty_string?(""), do: true
+  def empty_string?(s) when is_binary(s), do: false
+
 end
--- lib/aoc2023/day08.ex
+++ lib/aoc2023/day08.ex
@@ -3,8 +3,79 @@ defmodule Aoc2023.Day08 do
     File.read!("input/day08.txt")
   end
 
-  def part1(_data) do
-    # stub
-    0
+  def part1(data) do
+    {instructions, map} = parse_data(data)
+
+    instructions
+    |> Stream.cycle()
+    |> Stream.transform("AAA", fn
+      _, "ZZZ" ->
+        {:halt, "ZZZ"}
+
+      dir, vertex ->
+        edges = :digraph.out_edges(map, vertex)
+
+        {_, _, to, _} =
+          edges
+          |> Enum.map(fn e -> :digraph.edge(map, e) end)
+          |> Enum.find(fn
+            {_, _, _, [^dir]} -> true
+            _ -> false
+          end)
+
+        {[to], to}
+    end)
+    |> Enum.count()
+  end
+
+  defp parse_data(data) do
+    [instruction_string | map_strings] =
+      data
+      |> String.split("\n")
+      |> Enum.map(&String.trim/1)
+      |> Enum.reject(&Aoc2023.empty_string?/1)
+
+    instructions = parse_instructions(instruction_string)
+    map = parse_map(map_strings)
+
+    {instructions, map}
+  end
+
+  defp parse_instructions(s) do
+    for c <- String.codepoints(s) do
+      case c do
+        "L" -> :left
+        "R" -> :right
+      end
+    end
+  end
+
+  defp parse_map(map_strings) do
+    graph = :digraph.new()
+
+    map_strings
+    |> Enum.map(&parse_map_line/1)
+    |> Enum.reduce(graph, fn {from, left, right}, graph ->
+      :digraph.add_vertex(graph, from)
+      :digraph.add_vertex(graph, left)
+      :digraph.add_vertex(graph, right)
+
+      :digraph.add_edge(graph, from, left, [:left])
+      :digraph.add_edge(graph, from, right, [:right])
+
+      graph
+    end)
+  end
+
+  defp parse_map_line(line) do
+    [node, rest] = String.split(line, " = ", parts: 2)
+
+    [left, right] =
+      rest
+      |> String.trim_leading("(")
+      |> String.trim_trailing(")")
+      |> String.split(", ", parts: 2)
+
+    {node, left, right}
   end
 end
