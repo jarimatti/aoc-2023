e402ab01d314ced5ca2c592f414bdb840fd4893a
Jarimatti Valkonen <jarimatti@me.com>
2023-12-09 11:59:20 +0000

    Parse input.

--- lib/aoc2023/day09.ex
+++ lib/aoc2023/day09.ex
@@ -1,6 +1,13 @@
 defmodule Aoc2023.Day09 do
-  def part1(_data) do
-    # stub
-    0
+  def part1(data) do
+    parse_data(data)
+  end
+
+  defp parse_data(data) do
+    data
+    |> String.split("\n")
+    |> Enum.reject(&Aoc2023.empty_string?/1)
+    |> Enum.map(&String.split/1)
+    |> Enum.map(fn l -> Enum.map(l, &String.to_integer/1) end)
   end
 end
