23e4a9cbe909d0ecfab681c98a5b0582dd5ee1a9
Jarimatti Valkonen <jarimatti@me.com>
2023-12-09 12:08:35 +0000

    Day 9, part 1 solved.
    This is not tail recursive but does the job for part 1 nicely.

--- lib/aoc2023/day09.ex
+++ lib/aoc2023/day09.ex
@@ -1,6 +1,14 @@
 defmodule Aoc2023.Day09 do
+
+  def input() do
+    File.read!("input/day09.txt")
+  end
+
   def part1(data) do
-    parse_data(data)
+    data
+    |> parse_data()
+    |> Enum.map(&next_number/1)
+    |> Enum.sum()
   end
 
   defp parse_data(data) do
@@ -9,5 +17,25 @@ defmodule Aoc2023.Day09 do
     |> Enum.reject(&Aoc2023.empty_string?/1)
     |> Enum.map(&String.split/1)
     |> Enum.map(fn l -> Enum.map(l, &String.to_integer/1) end)
+  end
+
+  defp next_number(history) do
+    case done?(history) do
+      true ->
+        List.last(history)
+      false ->
+
+        next_number(delta(history)) + List.last(history)
+    end
+  end
+
+  defp done?(history) do
+    Enum.all?(history, fn x -> x == 0 end)
+  end
+
+  defp delta(history) do
+    history
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(fn [a, b] -> b - a end)
   end
 end
