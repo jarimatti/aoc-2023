18869192121838f7c9ed2f1c7f1009595813086c
Jarimatti Valkonen <jarimatti@me.com>
2023-12-10 15:43:20 +0000

    Day 10, part 1 stub.

---
+++ lib/aoc2023/day10.ex
@@ -0,0 +1,10 @@
+defmodule Aoc2023.Day10 do
+  def input() do
+    File.read!("input/day10.txt")
+  end
+
+  def part1(data) do
+    # stub
+    0
+  end
+end
---
+++ test/aoc2023/day10_test.exs
@@ -0,0 +1,58 @@
+defmodule Aoc2023.Day10Test do
+  use ExUnit.Case, async: true
+
+  alias Aoc2023.Day10
+
+  test "part 1 simple sample input" do
+    data =
+      """
+      .....
+      .S-7.
+      .|.|.
+      .L-J.
+      .....
+      """
+
+    assert Day10.part1(data) == 4
+  end
+
+  test "part 1 simle sample with extra pipes" do
+    data =
+      """
+      -L|F7
+      7S-7|
+      L|7||
+      -L-J|
+      L|-JF
+      """
+
+    assert Day10.part1(data) == 4
+  end
+
+  test "part 1 complex sample input" do
+    data =
+      """
+      ..F7.
+      .FJ|.
+      SJ.L7
+      |F--J
+      LJ...
+      """
+
+    assert Day10.part1(data) == 8
+  end
+
+  test "part 1 complex sample with extra pipes" do
+    data =
+      """
+      7-F7-
+      .FJ|7
+      SJLL7
+      |F--J
+      LJ.LJ
+      """
+
+    assert Day10.part1(data) == 8
+  end
+
+end
