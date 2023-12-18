95c50c9c7cf791607af0e03608e9c8eb93d18b5d
Jarimatti Valkonen <jarimatti@me.com>
2023-12-18 23:14:01 +0000

    Day 14, part 1: tilt north.

--- lib/aoc2023/day14.ex
+++ lib/aoc2023/day14.ex
@@ -2,9 +2,8 @@ defmodule Aoc2023.Day14 do
   def part1(data) do
     {map, ranges} = parse_data(data)
 
-    IO.puts(format_map(map, ranges))
-
     map
+    |> tilt_north(ranges)
   end
 
   defp parse_data(data) do
@@ -44,15 +43,56 @@ defmodule Aoc2023.Day14 do
   defp parse_char("#"), do: :block
   defp parse_char("O"), do: :ball
 
-  defp format_map(map, {x_range, y_range}) do
+  def format_map(map, {x_range, y_range}) do
     for y <- y_range do
       for x <- x_range do
         format_element(Map.get(map, {x, y}))
       end
-    end |> Enum.intersperse("\n")
+    end
+    |> Enum.intersperse("\n")
   end
 
   defp format_element(nil), do: "."
   defp format_element(:block), do: "#"
   defp format_element(:ball), do: "O"
+
+  defp tilt_north(map, {x_range, y_range}) do
+    # Go through each column
+    # Travel from top to bottom.
+    # Track latest free slot (first unfilled one) and if encountering ball, move it there.
+    # If encountering block or ball, start to move the free slot.
+
+    {map, _} =
+      Enum.reduce(x_range, {map, nil}, fn x, {map, _} ->
+        Enum.reduce(y_range, {map, :search_free}, fn
+          y, {map, :search_free} ->
+            case Map.get(map, {x, y}) do
+              nil -> {map, y}
+              _ -> {map, :search_free}
+            end
+
+          y, {map, latest_free_y} ->
+            case Map.get(map, {x, y}) do
+              nil ->
+                {map, latest_free_y}
+
+              :ball ->
+                from = {x, y}
+                to = {x, latest_free_y}
+                {move_ball(map, from, to), latest_free_y + 1}
+
+              :block ->
+                {map, :search_free}
+            end
+        end)
+      end)
+
+    map
+  end
+
+  defp move_ball(map, from, to) do
+    map
+    |> Map.put(to, :ball)
+    |> Map.delete(from)
+  end
 end
